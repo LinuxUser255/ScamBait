1121d87c01e7959f3ecca9427b37d87f3cfb247f
Chris <atnl@protonmail.com>
2025-01-22 02:51:19 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# import methods from scam_bait_bot
+from scammer_bait import ScamBait
+
+def run_bot():
+    ScamBait()
+
+def main():
+    run_bot()
+
+# Entry point of the bot
+if __name__ == '__main__':
+    main()
