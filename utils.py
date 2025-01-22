9c4ff39f4f87043689ef1d7d8d5e1b641a56f7c9
Chris <atnl@protonmail.com>
2025-01-22 03:01:14 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,37 @@
+"""
+Utilities
+This is a carry-over from my LinkedIn Easy Apply bot.
+Some of the functionality could be usefull in the future
+Leaving some of the code here for now for furture repurpose, ideas, etc..
+"""
+#   import math, constants, config
+#   from typing import List
+#   import time
+
+# Might need to add more utility functions here
+# Simialr to what is in LinkedIn_Apply
+# Then import that functionality into scammer_bait.py
+
+# However, utilities.py is that code-base, is specifiec to generating
+# Urls, based on settings in config.python
+# will keep this here for now
+
+#def get_loggedin_status():
+#    pass
+
+# use of the keywords arrary from config.py,found in utils.py
+# in the original LinkedIn_Apply bot
+# it was used as part of the url generation function
+# And the list within the array was iterated over to generate those URLs
+# Might need to add something similar in this bot..
+# def url_to_keywords() -> List[str]:
+#     keyword_var = var[var.index("keywords=") + 9:]
+#     keyword = keyword_var[0:keyword_var.index("&")]
+#     return [keyword]
+
+# class LinkedinUrlGenerate:
+#     def generate_var_foo(self) -> List[str]:
+#         path = []
+#         for location in config.location:
+#             for keyword in config.keywords:
+#                 var = constants.bot_url + "?f_AL=true&keywords="
