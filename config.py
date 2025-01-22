5bb7168447cdf5a098c7181c7739e1fc0cadacb4
Chris <atnl@protonmail.com>
2025-01-22 02:58:49 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# General Bot settings
+
+# Some of the following, are general template/examples, edit according to your needs.
+browser = ["Chrome"]
+
+# Optional! run browser in headless mode, no browser screen will be shown it will work in background.
+headless = False
+
+# LinkedIn login creds
+email = "youremai@mail.com"
+password = "yourpasswordhere"
+
+# These settings are for running the bot in various modes
+ScamBaitPassword = "yourlinkedinpasswordhere"
+
+# This array is used/referred to in the utils.py
+# Identifier words to single out fake recruiters
+hashtag_keywords = ["opentowork"]
+
+# #opentowork hashtag url
+openToWork = "https://www.linkedin.com/feed/hashtag/?keywords=opentowork"
+# displays posts with the opentowork hashtags
+
+# These arrarys my come in useful in the future.
+# Other fake profiles
+#fake_profile1 = "John Doe"
+#fake_profiles = ["fake_profile1", "fake_profile2", "fake_profile3"]
+sort = ["Relevant"]
