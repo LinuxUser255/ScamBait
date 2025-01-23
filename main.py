6960af5b0eb8bd98d0221a49ed0acf20df792119
Chris <atnl@protonmail.com>
2025-01-23 17:49:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,11 @@
 # import methods from scam_bait_bot
 from scammer_bait import ScamBait
 
-
 def run_bot():
     ScamBait()
 
-
 def main():
     run_bot()
-
 
 # Entry point of the bot
 if __name__ == '__main__':
